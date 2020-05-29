04b8238cd027912f902385966d99406a4ada9aa6
vinaykannambadi <vinaykannambadi@gmail.com>
2020-05-29 17:00:21 +0530

    jwt-interceptor

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -19,10 +19,59 @@ export class FakeBackendInterceptor implements HttpInterceptor {
     .pipe(delay(500))
     .pipe(dematerialize())
 
-    function handleRoute(){
+    function handleRoute(request){
       switch(true){
+        case url.endsWith('/users/authenticate') && method === 'POST':
+        return authenticate();
+
+        case url.endsWith('/users') && method === 'GET':
+                    return getUsers();
+        default:
+
+        return next.handle(request);
         
       }
     }
+
+    function authenticate(){
+      const {username, password} = body;
+      const user = users.find(x => x.username === username && x.password === password);
+      if(!user) return error('Username or password is incorrect')
+      return ok({
+                id: user.id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                token: 'fake-jwt-token'
+            })
+
+
+    }
+
+    function getUsers() {
+            if (!isLoggedIn()) return unauthorized();
+            return ok(users);
+    }
+
+    function ok(body?) {
+            return of(new HttpResponse({ status: 200, body }));
+        }
+
+        function error(message){
+          return throwError({ error: { message } });
+        }
+    function unauthorized() {
+            return throwError({ status: 401, error: { message: 'Unauthorised' } });
+        }
+        function isLoggedIn() {
+            return headers.get('Authorization') === 'Bearer fake-jwt-token';
+        }
   }
 }
+
+export let fakeBackendProvider = {
+    // use fake backend in place of Http service for backend-less development
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
+};
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -4,4 +4,5 @@ export class User{
   password: string;
   firstname: string;
   lastname: string;
+  token?: string;
 }
